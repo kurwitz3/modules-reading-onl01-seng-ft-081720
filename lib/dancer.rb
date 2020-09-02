041b8f246ab55f7ccf1a6e269c3675a8593da511
Richard Kurwitz <kurwitz3@aol.com>
2020-09-02 18:45:33 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,10 +2,7 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 class Dancer
-    include Dance
- extend MetaDancing
-  attr_accessor :name
- 
+  
   def initialize(name)
     @name = name
   end
