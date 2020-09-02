3bbd936bab75b7f6c1401af72fa5e1a485e7a32d
Richard Kurwitz <kurwitz3@aol.com>
2020-09-02 18:45:42 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,7 +2,8 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 class Dancer
-  
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
   def initialize(name)
     @name = name
   end
